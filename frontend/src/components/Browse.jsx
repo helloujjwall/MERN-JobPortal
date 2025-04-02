75fb42881c40cf16e540151459b49e0a0010e97c
helloujjwall <mrujjwal03@gmail.com>
2025-04-02 11:42:58 +0530

    start creating Profile Page

---
+++ frontend/src/components/Browse.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './shared/Navbar';
+import Job from './Job';
+
+const randomJobs = [1, 2, 3];
+
+function Browse() {
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto my-10 px-4">
+        <h1 className="text-2xl font-semibold mb-6">
+          Search Results: {randomJobs.length}
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {randomJobs.map((item, index) => (
+            <Job/>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Browse;
